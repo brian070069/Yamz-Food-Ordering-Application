5f1aab1435ff13471217c4745b02360a779b2813
brian070069 <134399957+brian070069@users.noreply.github.com>
2024-01-16 20:41:23 +0300

    Update BaseUrls.js

--- src/services/BaseUrls.js
+++ src/services/BaseUrls.js
@@ -4,9 +4,9 @@
 // export const profileApi = "http://127.0.0.1:8000/profile/";
 // export const charts = "http://127.0.0.1:8000/records/dailyrecord/";
 
-export const userUrl = "https://yuhmz-510557fdfff7.herokuapp.com/auth/";
-export const cartBaseUrl = "https://yuhmz-510557fdfff7.herokuapp.com/menu/";
-export const mpesaBaseUrl = "https://yuhmz-510557fdfff7.herokuapp.com/mpesa/";
-export const profileApi = "https://yuhmz-510557fdfff7.herokuapp.com/profile/";
+export const userUrl = "https://yahmz.com/auth/";
+export const cartBaseUrl = "https://yahmz.com/menu/";
+export const mpesaBaseUrl = "https://yahmz.com/mpesa/";
+export const profileApi = "https://yahmz.com/profile/";
 export const charts =
-  "https://yuhmz-510557fdfff7.herokuapp.com/records/dailyrecord/";
+  "https://yahmz.com/dailyrecord/";
